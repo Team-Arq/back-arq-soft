4c58b996294b2f44745543d9c1f7caf4a64eeca0
Carlos Carranza <carlosscarranza@yahoo.com>
2020-08-12 19:58:25 -0500

    chore: add middleware and api login

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
+const middleware = require('./middleware');
 const apiUsersRouter = require('./api/users');
 
+//router.use('/appointment, middleware.checkToken, apiAppointmentRouter');
 router.use('/users', apiUsersRouter);
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const { User } = require('../../db');
 const { check, validationResult } = require('express-validator');
+const moment = require('moment');
+const jwt = require('jwt-simple');
 
 router.post('/register',[
     check('username', 'El nombre de usuario es obligatorio').not().isEmpty(),
@@ -18,5 +20,29 @@ router.post('/register',[
     const user = await User.create(req.body);
     res.json(user);
 });
+
+router.post('/login', async (req, res) => {
+    const user = await User.findOne({where: { email: req.body.email }});
+    if(user){
+        const iguales = bcrypt.compareSync(req.body.password, user.password);
+        if(iguales){
+            res.json({success: createToken(user)});
+        } else {
+            res.json({error: 'Error en usuario y/o contraseña'})
+        }
+    } else {
+        res.json({error: 'Error en usuario y/o contraseña'})
+    }
+})
+
+const createToken = (user) => {
+    const payload = {
+        usuarioId: user.id,
+        createdAt: moment().unix(),
+        expiredAt: moment().add(5, 'minutes').unix()
+    }
+
+    return jwt.encode(payload, 'secret-phrase');
+}
 
 module.exports = router;
