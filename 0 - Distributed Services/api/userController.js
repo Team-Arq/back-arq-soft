a63f88f1c471dfb4a292275e409cac4c646344da
Carlos Carranza <carlosscarranza@yahoo.com>
2020-08-20 20:35:45 -0500

    move app to application

--- 0 - Distributed Services/api/userController.js
+++ 0 - Distributed Services/api/userController.js
@@ -1,48 +1,20 @@
 const router = require('express').Router();
-const bcrypt = require('bcryptjs');
-const { User } = require('../../3 - Infrastructure/db');
+
+const { UserApplication } = require('../../1 - Application/userApplication');
 const { check, validationResult } = require('express-validator');
-const moment = require('moment');
-const jwt = require('jwt-simple');
 
 router.post('/register',[
     check('username', 'El nombre de usuario es obligatorio').not().isEmpty(),
     check('password', 'El password es obligatorio').not().isEmpty(),
     check('email', 'El formato del email es incorrecto').isEmail()
 ], async (req, res) => {
-
     const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(422).json({errores: errors.array()})
-    }
-
-    req.body.password = bcrypt.hashSync(req.body.password, 10);
-    const user = await User.create(req.body);
-    res.json(user);
+    if(!errors.isEmpty()){ return res.status(422).json({errores: errors.array()}) }
+    await UserApplication.createUser(req, res);    
 });
 
 router.post('/login', async (req, res) => {
-    const user = await User.findOne({where: { email: req.body.email }});
-    if(user){
-        const iguales = bcrypt.compareSync(req.body.password, user.password);
-        if(iguales){
-            res.json({success: createToken(user)});
-        } else {
-            res.json({error: 'Error en usuario y/o contraseña'})
-        }
-    } else {
-        res.json({error: 'Error en usuario y/o contraseña'})
-    }
+    await UserApplication.loginUser(req, res); 
 })
-
-const createToken = (user) => {
-    const payload = {
-        usuarioId: user.id,
-        createdAt: moment().unix(),
-        expiredAt: moment().add(5, 'minutes').unix()
-    }
-
-    return jwt.encode(payload, 'secret-phrase');
-}
 
 module.exports = router;
---
+++ 1 - Application/userApplication.js
@@ -0,0 +1,35 @@
+const bcrypt = require('bcryptjs');
+const moment = require('moment');
+const jwt = require('jwt-simple');
+const { User } = require('../3 - Infrastructure/db');
+
+async function createUser (req, res) {
+    req.body.password = bcrypt.hashSync(req.body.password, 10);
+    const user = await User.create(req.body);
+    res.json(user);
+}
+
+async function loginUser (req, res) {
+    const user = await User.findOne({where: { email: req.body.email }});
+    if(user){
+        const iguales = bcrypt.compareSync(req.body.password, user.password);
+        if(iguales){
+            res.json({success: createToken(user)});
+        } else {
+            res.json({error: 'Error en usuario y/o contraseña'})
+        }
+    } else {
+        res.json({error: 'Error en usuario y/o contraseña'})
+    }
+}
+
+const createToken = (user) => {
+    const payload = {
+        usuarioId: user.id,
+        createdAt: moment().unix(),
+        expiredAt: moment().add(5, 'minutes').unix()
+    }
+
+    return jwt.encode(payload, 'secret-phrase');
+}
+
