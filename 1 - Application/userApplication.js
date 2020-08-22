a58cc4a699c72c0bb29d635d23bcdb078d9aa58a
Carlos Carranza <carlosscarranza@yahoo.com>
2020-08-22 09:42:34 -0500

    fix: edit user

--- 1 - Application/userApplication.js
+++ 1 - Application/userApplication.js
@@ -3,55 +3,61 @@ const moment = require('moment');
 const jwt = require('jwt-simple');
 const { User } = require('../3 - Infrastructure/db');
 
-async function CreateUser (req, res) {
+async function CreateUser(req, res) {
     req.body.password = bcrypt.hashSync(req.body.password, 10);
     const user = await User.create(req.body);
     res.json(user);
 }
 
-async function LoginUser (req, res) {
+async function LoginUser(req, res) {
 
-    const user = await User.findOne({where: { email: req.body.email }});
-    if(user){
+    const user = await User.findOne({ where: { email: req.body.email } });
+    if (user) {
         const iguales = bcrypt.compareSync(req.body.password, user.password);
-        if(iguales){
-            res.json({success: createToken(user)});
+        if (iguales) {
+            res.json({ success: createToken(user) });
         } else {
-            res.json({error: 'Error en usuario y/o contraseña'})
+            res.json({ error: 'Error en usuario y/o contraseña' })
         }
     } else {
-        res.json({error: 'Error en usuario y/o contraseña'})
+        res.json({ error: 'Error en usuario y/o contraseña' })
     }
 }
 
-async function GetUser (req, res){
+async function GetUser(req, res) {
 
-    const user = await User.findOne({where: { email: req.body.email }});
-    if(user){
-        res.json({success: user})
+    const user = await User.findOne({ where: { email: req.body.email } });
+    if (user) {
+        res.json({ success: user })
     }
-    else{
-        res.json({error: 'Error el usuario no existe'})
+    else {
+        res.json({ error: 'Error el usuario no existe' })
     }
 }
 
-async function EditUser (req, res){
-    await User.findOne({ where: { email: req.body.email } })
-    .on('success', function (user) {
-        // Check if record exists in db
-        if (user) {
-            user.update(
-                { username: req.body.username, password: req.body.password},
-                { where: { email: req.body.email } }
-            )
-            .then(function () {
-                res.json({success: 'Usuario Actualizado'})
-            })
-        }
-        else{
-            res.json({error: 'Error el usuario no existe'})
-        }
-    })
+async function EditUser(req, res) {
+
+    const user = await User.findOne({ where: { email: req.body.email } });
+
+    if (user) {
+        req.body.password = bcrypt.hashSync(req.body.password, 10);
+        User.update(
+            // Values to update
+            {
+                username:  req.body.username,
+                password: req.body.password
+            },
+            { // Clause
+                where: 
+                {
+                    email: req.body.email
+                }
+            }
+        ).then(() => { res.json({ success: user });}
+        ).catch((error) => { throw new Error(error)});
+    } else {
+        res.json({ error: 'Error el usuario no existe' })
+    }
 }
 
 const createToken = (user) => {
