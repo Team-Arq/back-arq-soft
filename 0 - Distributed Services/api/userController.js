322d04b9bab9757ef345e8e9b5f911d9e29563c9
Carlos Carranza <carlosscarranza@yahoo.com>
2020-08-22 08:42:15 -0500

    chore: user get and edit

--- 0 - Distributed Services/api/userController.js
+++ 0 - Distributed Services/api/userController.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const { loginUser, createUser } = require('../../1 - Application/userApplication');
+const { LoginUser, CreateUser, GetUser, EditUser } = require('../../1 - Application/userApplication');
 const { check, validationResult } = require('express-validator');
 
 router.post('/register',[
@@ -10,11 +10,25 @@ router.post('/register',[
 ], async (req, res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()){ return res.status(422).json({errores: errors.array()}) }
-    await createUser(req, res);    
+    await CreateUser(req, res);    
 });
 
 router.post('/login', async (req, res) => {
-    await loginUser(req, res); 
+    await LoginUser(req, res); 
+})
+
+router.get('/get-user', [
+    check('email', 'El formato del email es incorrecto').not().isEmpty()
+],async (req, res) => {
+    await GetUser(req, res); 
+})
+
+router.post('/edit-user', [
+    check('username', 'El nombre de usuario es obligatorio').not().isEmpty(),
+    check('password', 'El password es obligatorio').not().isEmpty(),
+    check('email', 'El formato del email es incorrecto').not().isEmpty()
+], async (req, res) => {
+    await EditUser(req, res); 
 })
 
 module.exports = router;
--- 1 - Application/userApplication.js
+++ 1 - Application/userApplication.js
@@ -3,13 +3,14 @@ const moment = require('moment');
 const jwt = require('jwt-simple');
 const { User } = require('../3 - Infrastructure/db');
 
-async function createUser (req, res) {
+async function CreateUser (req, res) {
     req.body.password = bcrypt.hashSync(req.body.password, 10);
     const user = await User.create(req.body);
     res.json(user);
 }
 
-async function loginUser (req, res) {
+async function LoginUser (req, res) {
+
     const user = await User.findOne({where: { email: req.body.email }});
     if(user){
         const iguales = bcrypt.compareSync(req.body.password, user.password);
@@ -23,6 +24,39 @@ async function loginUser (req, res) {
     }
 }
 
+async function GetUser (req, res){
+
+    const user = await User.findOne({where: { email: req.body.email }});
+    if(user){
+        res.json({success: user})
+    }
+    else{
+        res.json({error: 'Error el usuario no existe'})
+    }
+}
+
+async function EditUser (req, res){
+    await User.findOne({ where: { email: req.body.email } })
+    .on('success', function (user) {
+        // Check if record exists in db
+        if (user) {
+            user.update({
+                username: user.username,
+                password: user.password
+            })
+            .success(function () {
+                res.json({success: 'Usuario Actualizado'})
+            })
+            .error(function(){
+                res.json({error: 'Error el usuario no pudo ser actualizado'})
+            })
+        }
+        else{
+            res.json({error: 'Error el usuario no existe'})
+        }
+    })
+}
+
 const createToken = (user) => {
     const payload = {
         usuarioId: user.id,
@@ -33,5 +67,5 @@ const createToken = (user) => {
     return jwt.encode(payload, 'secret-phrase');
 }
 
-module.exports = { loginUser, createUser };
+module.exports = { LoginUser, CreateUser, GetUser, EditUser };
 
